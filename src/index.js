0d3b4198285ff83a18fa9443bb7cd2f323959303
mohamad <aghi.mohamad@gmail.com>
2022-05-15 08:00:11 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.getElementById("new-task-description")
+  const form = document.getElementById("create-task-form")
+  const ul = document.getElementById("tasks")
+  const color = document.getElementById("color")
+    form.addEventListener("submit", (e=>{
+    e.preventDefault()
+    const li = document.createElement("li")
+    li.innerHTML = input.value 
+    ul.append(li)
+    const del = document.createElement("button")
+    const edit = document.createElement("button")
+    edit.innerHTML = "E"
+    
+    del.innerHTML = "X"
+    del.style.marginLeft = "10px"
+    li.append(del)
+    li.append(edit)
+    li.style.color = `${color.value}`
+    input.value = ""
+    del.addEventListener("click", (e)=>{
+      li.remove()
+    })
+    edit.addEventListener("click", (e)=>{
+      const inputT = document.createElement("input")
+      inputT.value = li.innerText
+      li.style.display= "none"
+      li.parentNode.append(inputT)
+      document.addEventListener("keydown", (e)=>{
+        if (e.key === "Enter"){
+          li.textContent = inputT.value
+          li.style.display= "inline"
+          inputT.remove()
+          li.append(del)
+          li.append(edit)
+        }
+      })
+    })
+    
+
+  }))
+  
+  
+
 });
